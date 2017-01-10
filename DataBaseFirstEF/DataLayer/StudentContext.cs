eb7658e9dcb69158d37000680fbe0b64d4c095e4
Rafay Jamal <rafaynizami@gmail.com>
2017-01-10 01:01:31 -0600

    Add files via upload
    Student Context

---
+++ DataBaseFirstEF/DataLayer/StudentContext.cs
@@ -0,0 +1,23 @@
+﻿using DataBaseFirstEF.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirstEF.DataLayer
+{
+    public class StudentContext:DbContext
+    {
+        public StudentContext() : base("name=EF6RecipesConn") { }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().ToTable("Chapter3.Student");
+        }
+    }
+}
